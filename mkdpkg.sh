f04b1bab1a9d39da50fadc5076fababee4548124
root account <root@lgippup01>
2016-03-17 14:22:10 +0000

    tidy up files

--- mkdpkg.sh
+++ mkdpkg.sh
@@ -1,4 +1,4 @@
 git submodule init vendor/ck/
 git submodule update 
-gbp buildpackage --git-debian-branch=debian --git-submodules --git-upstream-branch=master --git-tag
+gbp buildpackage --git-debian-branch=debian --git-submodules --git-upstream-branch=master --git-tag $@
 gbp dch --debian-branch=debian
