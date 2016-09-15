1c81b406168553cbc2ecb74f91f709a7ea8e5aed
John Mylchreest <john.mylchreest@piksel.com>
2016-09-15 15:44:11 +0100

    add pre-req build packages into convenience build script

--- mkdpkg.sh
+++ mkdpkg.sh
@@ -1,3 +1,4 @@
+apt-get install libjansson-dev libmicrohttpd-dev libssl-dev
 git submodule init vendor/ck/
 git submodule update 
 gbp buildpackage --git-debian-branch=debian --git-submodules --git-upstream-branch=master --git-tag $@
